76475e369a501f59b38423936d6f8edc53339fad
Gilmarque <rodriguesgilmarques490@gmail.com>
2026-03-23 01:16:35 +0000

    Inicio

--- fup/bala/go/draft.go
+++ fup/bala/go/draft.go
@@ -1,5 +1,11 @@
 package main
 import "fmt"
+import "math"
 func main() {
-    fmt.Println("qxcode")
+    var x1, x2, y1, y2, distancia float64
+    fmt.Scan(&x1, &y1)
+    fmt.Scan(&x2, &y2)
+    
+    distancia = math.Sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1))
+    fmt.Printf("%.2f\n", distancia)
 }
