0f7688c8310c18f4ce35cf226abd5b10d86fb284
Gilmarque <rodriguesgilmarques490@gmail.com>
2026-03-24 17:59:23 +0000

    operações

--- fup/quebrada/go/draft.go
+++ fup/quebrada/go/draft.go
@@ -1,5 +1,15 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("qxcode")
+    var n1, n2 int
+    fmt.Scan(&n1, &n2)
+
+    resultado := n1 / n2
+    fmt.Printf("%d\n", resultado)
+    fmt.Printf("%d\n", n1 % n2)
+    var resultado2, n3, n4 float64
+    n3 = float64(n1)
+    n4 = float64(n2)
+    resultado2 = n3 / n4
+    fmt.Printf("%.2f\n", resultado2)
 }
