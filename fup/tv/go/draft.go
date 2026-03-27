ac56ce79191f101c7e6cf4d0ea87a86769aa5ca2
Gilmarque <rodriguesgilmarques490@gmail.com>
2026-03-27 22:53:39 +0000

    fiz a tv

--- fup/tv/go/draft.go
+++ fup/tv/go/draft.go
@@ -1,5 +1,15 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var valor float64
+    var parcelas int
+    fmt.Scan(&valor)
+    fmt.Scan(&parcelas)
+
+    juro := float64(parcelas-1) * 0.05
+    total := valor * (1 + juro)
+    parcela := total / float64(parcelas)
+
+    fmt.Printf("%.2f\n", parcela)
+    fmt.Printf("%.2f\n", total)
 }
