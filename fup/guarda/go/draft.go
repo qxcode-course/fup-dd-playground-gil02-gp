14e6c02aff1b00dda1208d5a89a89afc8ef36069
Gilmarque <rodriguesgilmarques490@gmail.com>
2026-03-28 17:55:09 +0000

    problemas primeiro

--- fup/guarda/go/draft.go
+++ fup/guarda/go/draft.go
@@ -1,5 +1,19 @@
 package main
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+    var wifi, login, admin int
+    fmt.Scan(&wifi)
+    fmt.Scan(&login)
+    fmt.Scan(&admin)
+
+    if(wifi == 0){
+        fmt.Printf("you must connect to wifi\n")
+    } else if(login == 0){
+        fmt.Printf("you need to login first\n")
+    }else if(admin == 0){
+        fmt.Printf("you must to login as admin\n")
+    } else{
+        fmt.Printf("done\n")
+    }
 }
