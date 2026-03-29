7be1e94c7d3b066545ddb97d3e0469d105050976
Gilmarque <rodriguesgilmarques490@gmail.com>
2026-03-29 19:22:19 +0000

    meio

---
+++ fup/meio/go/draft.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main() {
+    var n1, n2, n3 int
+    fmt.Scan(&n1)
+    fmt.Scan(&n2)
+    fmt.Scan(&n3)
+
+    if(n1 > n2 && n1 < n3) || (n1 < n2 && n1 > n3){
+        fmt.Println(n1)
+    } else if (n2 > n1 && n2 < n3) || (n2 < n1 && n2 > n3){
+        fmt.Println(n2)
+    } else {
+        fmt.Println(n3)
+    }
+}
