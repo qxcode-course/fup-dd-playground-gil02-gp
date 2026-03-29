46a8b11c63942d268123859ad3aace9fc67cde8e
Gilmarque <rodriguesgilmarques490@gmail.com>
2026-03-29 19:48:21 +0000

    jokenpo

--- fup/jokenpo/go/draft.go
+++ fup/jokenpo/go/draft.go
@@ -1,5 +1,17 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var jog1, jog2 string
+    fmt.Scan(&jog1)
+    fmt.Scan(&jog2)
+
+    if jog1 == jog2{
+        fmt.Println("empate")
+    } else if(jog1 == "R" && jog2 == "S") ||
+    (jog1 == "S" && jog2 == "P") || (jog1 == "P" && jog2 == "R"){
+        fmt.Println("jog1")
+    } else if(jog2 == "R" && jog1 == "S") ||
+    (jog2 == "S" && jog1 == "P") || (jog2 == "P" && jog1 == "R"){
+        fmt.Println("jog2")
+    }
 }
