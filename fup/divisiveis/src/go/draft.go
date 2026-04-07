621bc70d74e01bb1d38c9b5a0fef4c647ec76ca4
Gilmarque <rodriguesgilmarques490@gmail.com>
2026-04-07 17:02:48 +0000

    divisiveis

---
+++ fup/divisiveis/src/go/draft.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main() {
+    var a, b int
+    fmt.Scan(&a, &b)
+    if a % 3 == 0 && b % 3 == 0 || a % 5 == 0 && b % 5 == 0 {
+        fmt.Println("sim")
+    } else {
+        fmt.Println("nao")
+    }
+}
