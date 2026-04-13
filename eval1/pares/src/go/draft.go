540ec7ddfb4049be807975452cbd6582a276d4ef
Gilmarque <rodriguesgilmarques490@gmail.com>
2026-04-13 18:20:07 +0000

    pares

---
+++ eval1/pares/src/go/draft.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+func main() {
+    var a, b int
+    fmt.Scan(&a, &b)
+    
+    if a > b{
+        fmt.Println("invalido")
+        return
+    }
+    soma := 0
+    for i := a; i <= b; i++ {
+        if i % 2 == 0 {
+            soma += i
+        }
+    }
+    fmt.Printf("%d\n", soma)
+}
