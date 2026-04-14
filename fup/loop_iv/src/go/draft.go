0fc1464366844cbd8ee36583fe69ca48b28d63e2
Gilmarque <rodriguesgilmarques490@gmail.com>
2026-04-14 23:02:39 +0000

    loop_iv

--- fup/loop_iv/src/go/draft.go
+++ fup/loop_iv/src/go/draft.go
@@ -4,8 +4,12 @@ func main() {
     var a, b int
     fmt.Scan(&a, &b)
     fmt.Printf("[ ")
-    if b != a {
-        for i := a; i < b; i++ {
+    step := 1
+    if a > b {
+        step = -1
+    }
+    if a != b {
+        for i := a; i != b; i += step {
             fmt.Printf("%d ", i)
         }
     }
