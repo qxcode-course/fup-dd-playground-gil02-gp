15ebb796c4978e918d654993dc639daefa1feccc
Gilmarque <rodriguesgilmarques490@gmail.com>
2026-05-12 01:02:43 +0000

    dindin

---
+++ fup/dindin/src/go/draft.go
@@ -0,0 +1,40 @@
+package main
+import "fmt"
+func main() {
+    var n int
+    fmt.Scan(&n)
+
+    var sabor, turno string
+    chocolate := 0
+    limao := 0
+    manha := 0
+    tarde := 0
+
+    for i := 0; i < n; i++ {
+        fmt.Scan(&sabor, &turno)
+        if sabor == "c" {
+            chocolate++
+        } else if sabor == "l" {
+            limao++
+        }
+        if turno == "m" {
+            manha++
+        } else if turno == "t" {
+            tarde++
+        }
+    }
+    if chocolate > limao {
+        fmt.Println("c")
+    } else if limao > chocolate {
+        fmt.Println("l")
+    } else {
+        fmt.Println("empate")
+    }
+    if manha < tarde {
+        fmt.Println("m")
+    } else if tarde < manha {
+        fmt.Println("t")
+    } else {
+        fmt.Println("empate")
+    }
+}
