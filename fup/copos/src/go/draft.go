237b00975c8df4201a750cb362411f15a3adad71
Gilmarque <rodriguesgilmarques490@gmail.com>
2026-05-26 22:23:10 +0000

    copos

---
+++ fup/copos/src/go/draft.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+func main() {
+    var n int
+    fmt.Scan(&n)
+
+    for i := 1; i <= n; i++ {
+
+        for j := 1; j <= n-i; j++ {
+            fmt.Print(".")
+        }
+        for j := 1; j <= i; j++ {
+            fmt.Print(n)
+            if j < i {
+                fmt.Print(".")
+            }
+        }
+        for j := 1; j <= n-i; j++ {
+            fmt.Print(".")
+        }
+        fmt.Println()
+    }
+}
